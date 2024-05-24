eecc90c9069c60efcb797d66112a1d57a32ead83
Denis <denisdenis668@yahoo.com>
2024-05-24 21:42:07 +0300

    <p> design

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div className="bg-gray-100 h-screen flex items-center justify-center">
+    <div className="bg-gray-100 h-screen flex flex-col items-center justify-center">
       <h1 className="color-blue-500 text-5xl">Language Learning Assistant 📚🗣️</h1>
-      <p>clujhack 2024</p>
+      <p className="color-gray-300 text-4xl mt-4">Cluj Hack 2024</p>
     </div>
   )
 }
