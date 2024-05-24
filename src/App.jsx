cced12cf1ee9d7072db1c22327f07a52d2d4becd
Denis <denisdenis668@yahoo.com>
2024-05-24 21:22:48 +0300

    react@vite

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="bg-gray-100 h-screen flex items-center justify-center">
+      <h1 className="color-blue-500 text-5xl">Language Learning Assistant 📚🗣️</h1>
+    </div>
+  )
+}
+export default App;
