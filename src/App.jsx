59266791d908bff5159f9cea094cb949c1fb1e1d
Denis <denisdenis668@yahoo.com>
2024-05-24 21:31:47 +0300

    add <P>

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div className="bg-gray-100 h-screen flex items-center justify-center">
       <h1 className="color-blue-500 text-5xl">Language Learning Assistant 📚🗣️</h1>
+      <p>Cluj Hack 2024</p>
     </div>
   )
 }
