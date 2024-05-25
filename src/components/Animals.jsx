0681328a701b8be26cdb009db29c50d8d021d052
TudorPavalau <tudorstefanpavalau@gmail.com>
2024-05-25 09:54:50 +0300

    video

--- src/components/Animals.jsx
+++ src/components/Animals.jsx
@@ -5,8 +5,8 @@ const Animals = () => {
         <div className="h-screen w-full rounded-md relative flex flex-col items-center justify-center antialiased" style={{background: 'linear-gradient(to right, #000000, #222222)'}}>
             <div className="max-w-5xl mx-auto p-4 flex flex-row justify-center items-center">
                 <div>
-                <h1 className="relative z-10 text-xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-                    Denis Denis
+                <h1 className="relative z-10 text-xl md:text-4xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+                    Choose your favourite talking peer
                 </h1>
                 </div>
             </div>
