59a0d9a0a3a079a6e4ec1aa611c63936a80c426c
Bolohan <bolohanandrei769@gmail.com>
2024-05-25 11:23:18 +0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Hero from "./components/Hero";
 import Animals from "./components/Animals";
+import Navbar from "./components/navbar";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Hero />
-      <Animals />
-      {/* <1 />
-      <2 /> */}
-    </BrowserRouter>
+    <main>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Hero/>} />
+          <Route path="/Hero" element={<Hero/>} />
+          <Route path="/Animals" element={<Animals />} />
+        </Routes>
+      </Router>
+    </main>
   )
 }
-
 export default App;
